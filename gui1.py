bd8397605809db3077a05322291b1761c067f830
Bmbo-sprg <wangpaiduyao@gmail.com>
2024-01-19 01:13:13 +0900

    1

--- gui1.py
+++ gui1.py
@@ -18,15 +18,6 @@ from utils import (
     spec2chroma,
 )
 
-'''
-AudioVisualizer
-    |--- __init__ <- ガチの最初に呼ばれる (ウィジェットをとりあえずpackし、figは作らない (音声がNoneなので))
-    |--- load_file <- ファイルを読んで、figを初期化する (表示範囲は最大)
-    |--- initialize_fig <- いる？
-    |--- reload_fig <- 表示範囲を更新する
-    |--- タイマー関連 <- 100msをchunkとして、サンプルコードをパクる
-    |--- 音声再生関連
-'''
 
 class AudioVisualizer(ttk.Frame):
     def __init__(self, master=None):
@@ -59,8 +50,9 @@ class AudioVisualizer(ttk.Frame):
         self.fft_size_frame.pack(side=tk.TOP)
         self.fft_size_label = ttk.Label(self.fft_size_frame, text='FFT size')
         self.fft_size_label.pack(side=tk.LEFT)
-        self.fft_size_entry = ttk.Entry(self.fft_size_frame)
-        self.fft_size_entry.insert(tk.END, '4096')
+        self.fft_size_entry = ttk.Combobox(
+            self.fft_size_frame, values=[2 ** i for i in range(9, 15)])
+        self.fft_size_entry.set('4096')
         self.fft_size_entry.pack(side=tk.LEFT, anchor=tk.W)
 
         self.fft_shift_frame = ttk.Frame(self.lframe, padding=(0, 10))
@@ -86,6 +78,14 @@ class AudioVisualizer(ttk.Frame):
         self.sr_label = ttk.Label(self.lframe, text='')
         self.sr_label.pack(side=tk.TOP)
 
+        self.quit_button = ttk.Button(
+            self.lframe,
+            text='Quit app',
+            bootstyle=ttk_const.DANGER,
+            command=self.quit,
+        )
+        self.quit_button.pack(side=tk.TOP)
+
         self.frame_wave = ttk.Frame(self)
         self.frame_wave.pack(side=tk.TOP)
         self.fig_wave = plt.figure(figsize=(16, 3))
@@ -102,7 +102,7 @@ class AudioVisualizer(ttk.Frame):
 
         self.ctrl_frame = ttk.Frame(self)
         self.ctrl_frame.pack(side=tk.TOP)
-        self.time_label = ttk.Label(self.ctrl_frame, text='00:00:00')
+        self.time_label = ttk.Label(self.ctrl_frame, text=self._format_time(0))
         self.time_label.pack(side=tk.LEFT)
         self.play_button = ttk.Button(
             self.ctrl_frame,
@@ -126,7 +126,74 @@ class AudioVisualizer(ttk.Frame):
         )
         self.stop_button.pack(side=tk.LEFT)
 
-    def load_file(self):
+    def reload_fig(self):
+        # do some re-scaling stuff
+
+        self.fig_wave.clf()
+        ax = self.fig_wave.add_subplot(111)
+        ax.set_xlim([0, len(self.wave)])
+        ax.xaxis.set_visible(False)
+        self.img_wave = ax.plot(self.wave)
+
+        self.fig_specgram.clf()
+        ax = self.fig_specgram.add_subplot(111)
+        ax.set_yscale('log')
+        ax.set_ylim([60, self.sr / 2])
+        self.img_specgram = ax.imshow(
+            self.specgram,
+            extent=[0, len(self.specgram), 0, self.sr / 2],
+            aspect='auto',
+            interpolation='nearest',
+            cmap='inferno',
+        )
+        self.img_f0 = ax.plot(self.f0, label='f0', color='red')
+        ax = ax.twinx()
+        ax.set_ylim([0, 23])
+        self.img_chordgram = ax.plot(self.chordgram, label='chord', color='blue')
+
+        self.canvas_wave.draw()
+        self.canvas_specgram.draw()
+
+    def play(self):
+        if self.is_playing:
+            return
+        self.is_playing = True
+        self.play_button.configure(bootstyle=ttk_const.SECONDARY)
+        self.pause_button.configure(bootstyle=ttk_const.WARNING)
+        self.increment()
+
+    def increment(self):
+        if not self.is_playing:
+            return
+        self.play_ms += 100
+        self.time_label.configure(text=self._format_time(self.play_ms))
+        self.img_wave[0].set_xdata(np.arange(0, len(self.wave)))
+        self.img_wave[0].set_ydata(self.wave)
+        self.img_specgram.set_data(self.specgram)
+        self.img_f0[0].set_ydata(self.f0)
+        self.img_chordgram[0].set_ydata(self.chordgram)
+        self.canvas_wave.draw_idle()
+        self.canvas_specgram.draw_idle()
+        self.master.after(100, self.increment)
+
+    def pause(self):
+        if not self.is_playing:
+            return
+        self.is_playing = False
+        self.play_button.configure(bootstyle=ttk_const.SUCCESS)
+        self.pause_button.configure(bootstyle=ttk_const.SECONDARY)
+
+    def stop(self):
+        self.is_playing = False
+        self.play_ms = 0
+        self.play_button.configure(bootstyle=ttk_const.SUCCESS)
+        self.pause_button.configure(bootstyle=ttk_const.SECONDARY)
+        self.time_label.configure(text=self._format_time(self.play_ms))
+
+    def open_file(self):
+        fTyp = [("wav file", "*.wav")]
+        iDir = os.path.abspath(os.path.dirname(__file__))
+        self.filename = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
         if self.filename == '':
             return
 
@@ -147,7 +214,9 @@ class AudioVisualizer(ttk.Frame):
         vol_threshold = int(self.vol_threshold_entry.get())
 
         self.wave, self.sr = librosa.load(self.filename, sr=None)
-        self.specgram = wave2specgram(self.wave, fft_size, fft_shift)
+        specgram = wave2specgram(self.wave, fft_size, fft_shift)
+        log_specgram = np.log(np.abs(specgram) + 1e-10)  # avoid log(0)
+        self.specgram = np.flipud(np.array(log_specgram).T)
 
         f0 = []
         chordgram = []
@@ -176,92 +245,20 @@ class AudioVisualizer(ttk.Frame):
         self.chordgram = np.asarray(chordgram)
 
         self.filename_label.configure(text=self.filename)
-        self.duration_label.configure(text=f'Duration: {len(self.wave) / self.sr:.2f} sec')
+        self.duration_label.configure(
+            text=f'Duration: {self._format_time(len(self.wave) / self.sr * 1000)}')
         self.sr_label.configure(text=f'Sampling rate: {self.sr} Hz')
-        self.initialize_fig()
-        # self.update_plot()
+        self.reload_fig()
 
-    def initialize_fig(self):
-        self.fig_wave.clf()
-        self.fig_specgram.clf()
-        ax = self.fig_wave.add_subplot()
-        ax.xaxis.set_visible(False)
-        self.plot_wave = ax.plot(self.wave)
-        ax = self.fig_specgram.add_subplot()
-        ax.set_yscale('log')
-        self.plot_specgram = ax.pcolormesh(
-            np.flipud(np.transpose(np.log(np.abs(self.specgram)))),
-            shading='nearest',
-            cmap='inferno',
-        )
+    def quit(self):
+        self.master.quit()
+        self.master.destroy()
 
-    def play(self):
-        if self.is_playing:
-            return
-        self.is_playing = True
-        self.play_button.configure(bootstyle=ttk_const.SECONDARY)
-        self.pause_button.configure(bootstyle=ttk_const.WARNING)
-        self.increment()
-
-    def increment(self):
-        if not self.is_playing:
-            return
-        self.play_ms += 100
-        m = self.play_ms // 1000 // 60
-        s = self.play_ms // 1000 % 60
-        ms = self.play_ms // 10 % 100
-        self.time_label.configure(text=f'{m:02}:{s:02}:{ms:02}')
-        self.master.after(100, self.increment)
-
-    def pause(self):
-        if not self.is_playing:
-            return
-        self.is_playing = False
-        self.play_button.configure(bootstyle=ttk_const.SUCCESS)
-        self.pause_button.configure(bootstyle=ttk_const.SECONDARY)
-
-    def stop(self):
-        self.is_playing = False
-        self.play_ms = 0
-        self.play_button.configure(bootstyle=ttk_const.SUCCESS)
-        self.pause_button.configure(bootstyle=ttk_const.SECONDARY)
-        self.time_label.configure(text='00:00:00')
-
-    def open_file(self):
-        fTyp = [("wav file", "*.wav")]
-        iDir = os.path.abspath(os.path.dirname(__file__))
-        self.filename = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
-        self.load_file()
-
-    def update_plot(self):
-        if self.wave is not None:
-            ax = self.fig_wave.add_subplot(111)
-            ax.set_xlim([0, len(self.wave)])
-            ax.xaxis.set_visible(False)
-            ax.plot(self.wave)
-            self.canvas_wave.draw()
-
-        if self.specgram is not None:
-            log_specgram = np.log(np.abs(self.specgram) + 1e-10)  # avoid log(0)
-            ax = self.fig_specgram.add_subplot(111)
-            ax.set_yscale('log')
-            ax.set_ylim([60, self.sr / 2])
-            ax.imshow(
-                np.flipud(np.array(log_specgram).T),
-                extent=[0, len(log_specgram), 0, self.sr/2],
-                aspect='auto',
-                interpolation='nearest',
-                cmap='inferno',
-            )
-
-            if self.f0 is not None:
-                ax.plot(self.f0, label='f0', color='red')
-
-            if self.chordgram is not None:
-                ax2 = ax.twinx()
-                ax2.set_ylim([0, 23])
-                ax2.plot(self.chordgram, label='chord', color='blue')
-            self.canvas_specgram.draw()
+    def _format_time(self, ms):
+        m = int(ms // 1000 // 60)
+        s = int(ms // 1000 % 60)
+        ms = int(ms // 10 % 100)
+        return f'{m:02}:{s:02}:{ms:02}'
 
 
 if __name__ == '__main__':
